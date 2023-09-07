f97300d3a9f1605a70de99d63f724f7a70f3cc7d
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-09-07 12:21:08 -0700

    modules imported

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """
+    a function ret div, mul, add & sub
+    """
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print("{} + {} = {}" .factor(a, b, add(a, b)))
+    print("{} - {} = {}" .factor(a, b, sub(a, b)))
+    print("{} * {} = {}" .factor(a, b, mul(a, b)))
+    print("{} / {} = {}" .factor(a, b, div(a, b)))
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """a function that imports and prints a from variable_load_5.py"""
+    from variable_load_5 import a
+
+    print(a)
