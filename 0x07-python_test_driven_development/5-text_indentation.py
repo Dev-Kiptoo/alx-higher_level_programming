6a0cf10b2ee5567865dba1b1d16929152630d5c0
Bethwel Kiptoo <kiptoobethwel001@gmail.com>
2023-10-06 13:57:34 -0700

    test driven development

--- 0x07-python_test_driven_development/5-text_indentation.py
+++ 0x07-python_test_driven_development/5-text_indentation.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
-def print_square(size):
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    for index in range(size):
-        print("x" * size)
+"""Module for text_indentation method."""
+
+
+def text_indentation(text):
+    """Method for adding 2 new lines after '.?:' chars.
+
+    Args:
+        text: The str text.
+
+    Raises:
+        TypeError: If text is not a str.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ".?:":
+        # print(delim, text.split(delim))
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
