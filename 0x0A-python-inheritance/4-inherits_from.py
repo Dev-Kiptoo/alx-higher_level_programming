6d1d24c6fc80e6570297ebff44b0fab42eb4249b
Bethwel <kiptoobethwel001@gmail.com>
2023-10-10 01:24:34 +0000

    Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-===================================
-module with method is_kind_of_class
-===================================
-"""
+"""Module containing inherits_from method"""
 
 
-def is_kind_of_class(obj, a_class):
-    """Method that return True if an object is an instance of a class
-    that inherited from"""
-
-    return isinstance(obj, a_class)
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False"""
+    return isinstance(obj, a_class) and type(obj) != a_class
