eae3f3b53d291ed7b14b29a7a3694b7dbb5b9c99
Bethwel <kiptoobethwel@01>
2023-11-15 23:16:14 +0300

    added the create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- creating the user_0b_1 having access to the server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
