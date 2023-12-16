1830ddad8a0128318c0e33b1778a653baf2802d9
Bethwel <kiptoobethwel001@gmail.com>
2023-12-16 17:34:19 +0300

    javascript warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 // a script that prints argument not found, argument found and arguments found
-if (process.argv.length === 2) {
-  console.log('Argument not found');
-} else if (process.argv.length === 1) {
-  console.log('Argument found');
-} else { console.log('Argument found'); }
+f (process.argv.length === 2) {
+	  console.log('No argument');
+} else if (process.argv.length === 3) {
+	  console.log('Argument found');
+} else {
+	  console.log('Arguments found');
+}
