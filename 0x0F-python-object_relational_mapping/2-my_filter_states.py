8cfc0c2ab53d50b633786172dd069cc64bb490f5
Bethwel <kiptoobethwel001@gmail.com>
2023-12-25 10:30:42 +0300

    added

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 """
-An argument that takes four arguments and returns the table for the argv{4}
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
 """
 import MySQLdb
-import sqlalchemy
 from sys import argv
 
-if __name__ == "__main__":
-    conn = MySQLdb.connect(host = "localhost", port = 3306, user = argv[1], passwd = argv[2], db = argv[3], charset = "utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-cur.close()
-conn.close()
+# The code should not be executed when imported
+if __name__ == '__main__':
+
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
