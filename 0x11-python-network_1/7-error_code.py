933d936bafd6fabcdcb58de786de79394d47deef
Bethwel <kiptoobethwel001@gmail.com>
2024-01-08 01:37:44 +0300

    7-error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,12 +7,15 @@ You must use the packages requests and sys
 You are not allowed to import packages other than requests and sys
 You don’t need to check arguments passed to the script (number or type)
 """
+import sys
 import requests
-from sys import argv
+
 
 if __name__ == "__main__":
-   request = requests.get(argv[1])
-   if requests.status_code >= 400:
-       print("Error code: {}".format(request.status_code))
-   else:
-        print(request.text)
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
